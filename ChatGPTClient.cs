133a12ec6dadf006f8a4ebbac1e223c1c1d8dc0d
Tamogna84 <naumalval@rambler.ru>
2023-10-08 18:17:51 +0500

    Initial commit

---
+++ ChatGPTClient.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using OpenAI.ChatGpt;
+using OpenAI.ChatGpt.Models.ChatCompletion.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTelegramChatGPT
+{
+    internal class ChatGPTClient
+    {
+        public async Task<string> Ask(string text)
+        {    
+           
+            
+            string? key = Environment.GetEnvironmentVariable("openai_api_key");
+            if (key == null)
+            {
+                throw new InvalidOperationException("Переменная окружения openai_api_key не задана!");
+            }
+
+            using var client = new OpenAiClient(key);
+
+                       
+            string response = await client.GetChatCompletions(new UserMessage(text), maxTokens: 1000);
+            
+            return response;
+        }
+
+    }
+}
